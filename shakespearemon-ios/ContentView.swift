61f6aa55d895a70030d308767c38809891ceda47
Fabrizio Scarano <scaranofbr@gmail.com>
2025-04-29 19:51:05 +0200

    Initial Commit

---
+++ shakespearemon-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shakespearemon-ios
+//
+//  Created by Fabrizio Scarano on 29/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shakespearemon-ios/shakespearemon_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shakespearemon_iosApp.swift
+//  shakespearemon-ios
+//
+//  Created by Fabrizio Scarano on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct shakespearemon_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

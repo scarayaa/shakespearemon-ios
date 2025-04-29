0a458396949339f246f7e6d05ed6c371fb8394d5
Fabrizio Scarano <scaranofbr@gmail.com>
2025-04-29 20:06:32 +0200

    feat: Updated initial files and classes naming

--- Shakespearemon/ShakespearemonApp.swift
+++ Shakespearemon/ShakespearemonApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct shakespearemon_iosApp: App {
+struct ShakespearemonApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
--- ShakespearemonTests/ShakespearemonTests.swift
+++ ShakespearemonTests/ShakespearemonTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import shakespearemon_ios
 
-final class shakespearemon_iosTests: XCTestCase {
+final class ShakespearemonTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
